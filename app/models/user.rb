0ce289afadc4f13672b0339d249abe18dace9837
nghiadk <nghiadk1102@gmail.com>
2017-12-08 19:41:34 +0700

    ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def self.find_user id
+    User.find_by id: id
+  end
 end
